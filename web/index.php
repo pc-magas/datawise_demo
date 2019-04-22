34c5fb3c1f108fcc01080a018fa2b67b7f207c20
Dimitrios Desyllas (pc_magas) <you@example.com>
2019-04-22 13:11:39 +0300

    Routing

--- web/index.php
+++ web/index.php
@@ -11,10 +11,12 @@ use GuzzleHttp\Client;
 $guzzle=new Client();
 $dropbox=DropboxFactory::fromIniFile(__DIR__."/../params.ini", $guzzle);
 
+// Frontpage in order to render the login button into the url.
 Flight::route('/', function(){
     Flight::render(VIEWS_DIR.'/frontpage.html.php',['login_url'=>'http://example.com']);
 });
 
+// Route that lists the files
 Flight::route('/files', function(){
     echo 'hello world!';
 });
